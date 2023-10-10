70a2df28871faadc3b56a7bea97f85b8cd6e9721
Clement <dumasclementjulien@yahoo.fr>
2023-10-10 11:42:00 +0200

    Footer en cours

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import './styles/App.css';
 import Header from './components/Header';
-// import SectionQuiSommesNous from './components/SectionQuiSommesNous';
 // import SectionPartenaires from './components/SectionPartenaires';
 // import SectionNosValeurs from './components/SectionNosValeurs';
 // import SectionNosCategories from './components/SectionNosCategories';
 // import SectionNosIncontournables from './components/SectionNosIncontournables';
-// import Footer from './components/Footer';
+// import WhoAreWe from './components/WhoAreWe';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <SectionQuiSommesNous />
-      <SectionPartenaires />
+       {/* <WhoAreWe /> */}
+      {/*<SectionPartenaires />
       <SectionNosValeurs />
       <SectionNosCategories />
-      <SectionNosIncontournables />
-      <Footer /> */}
+      <SectionNosIncontournables />*/}
+      <Footer /> 
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Footer = () => {
+
+    const footerStyle = {
+        backgroundColor: "#00988E", // Définit la couleur de fond
+        color: "white", // Définit la couleur du texte en blanc
+      };
+
+  return (
+    <footer className="page-footer" style={footerStyle}>
+          <div className="container">
+            <div className="row">
+            <div className="col l4 offset-l2 s12">
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Conditions générales de vente</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Données personnelles</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Mentions légales</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Nous contacter</a></li>
+                </ul>
+              </div>
+              <div className="col l6 s12">
+                <h5 className="white-text">Rejoignez la team Papou !</h5>
+                
+              </div>
+              
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2023 Copyright Text
+            </div>
+          </div>
+        </footer>
+  );
+};
+
+export default Footer;
