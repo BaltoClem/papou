4d0f52c5af8a3d2f8c38af42bad9a87490f03f36
Clement <dumasclementjulien@yahoo.fr>
2023-10-13 10:37:30 +0200

    OurEssentials terminé, à valider

--- src/components/OurEssentials.js
+++ src/components/OurEssentials.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import toy1 from '../data/fake_toys_papou/toy_1.jpg';
+import toy2 from '../data/fake_toys_papou/toy_2.jpg';
+import toy3 from '../data/fake_toys_papou/toy_3.jpg';
+import toy4 from '../data/fake_toys_papou/toy_4.jpg';
+import toy5 from '../data/fake_toys_papou/toy_5.jpg';
+import toy6 from '../data/fake_toys_papou/toy_6.jpg';
 
 const responsive = {
   superLargeDesktop: {
@@ -23,44 +29,49 @@ const responsive = {
 
 const carouselItems = [
   {
-    image: 'images/office.jpg',
+    image: toy1,
     seller: 'Nom du vendeur 1',
     price: '$10',
   },
   {
-    image: 'images/office.jpg',
+    image: toy2,
     seller: 'Nom du vendeur 2',
     price: '$20',
   },
   {
-    image: 'images/office.jpg',
+    image: toy3,
     seller: 'Nom du vendeur 3',
     price: '$30',
   },
   {
-    image: 'images/office.jpg',
+    image: toy4,
     seller: 'Nom du vendeur 4',
     price: '$40',
   },
   {
-    image: 'images/office.jpg',
+    image: toy5,
     seller: 'Nom du vendeur 5',
     price: '$50',
   },
   {
-    image: 'images/office.jpg',
+    image: toy6,
     seller: 'Nom du vendeur 6',
     price: '$60',
   }
 ];
 
+const ourEssentialsStyle = {
+  color: '#00989E'
+}
+
 const carouselContainerStyle = {
   margin: '20px', // Ajoutez la marge souhaitée
 };
 
-const ourEssentialsStyle = {
-  color: '#00989E'
-}
+const imageStyle = {
+  width: '100%', // La largeur de l'image sera de 100% du conteneur
+  height: '250px', // La hauteur de l'image est fixée à 200px (ajustez selon vos besoins)
+};
 
 const OurEssentials = () => {
   return (
@@ -71,7 +82,11 @@ const OurEssentials = () => {
         {carouselItems.map((item, index) => (
           <div key={index} className="card" style={{ margin: '50px' }}>
             <div className="card-image waves-effect waves-block waves-light">
-              <img className="activator" src={item.image} alt={`Produit ${index + 1}`} />
+              <img 
+              className="activator" 
+              src={item.image} 
+              alt={`Produit ${index + 1}`}
+              style={imageStyle} />
             </div>
             <div className="card-content">
               <span className="card-title activator grey-text text-darken-4">
