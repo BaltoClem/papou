67a489d5e68bedfdec59b2d1aa753176d69aec73
Clement <dumasclementjulien@yahoo.fr>
2023-10-17 15:39:21 +0200

    Modifications header + responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const headerStyle = {
     backgroundColor: "#00988E", // Définit la couleur de fond
     color: "white", // Définit la couleur du texte en blanc
-};
+  };
 
   return (
     <header style={headerStyle}>
@@ -16,26 +16,48 @@ const Header = () => {
       <div className="slogan">
         <p>Les jouets d'hier pour un avenir plus vert</p>
       </div>
-      <div className='search_icons'>
-        <nav>
-          <div className="nav-wrapper">
-            <form>
-              <div className="input-field">
-                <input id="search" type="search" placeholder='Recherche' required />
-                <label className="label-icon" for="search">
-                  <i className="material-icons">search</i>
-                </label>
-                <i className="material-icons">close</i>
-              </div>
-            </form>
-          </div>
-        </nav>
+
+      {/* General Bar */}
+      <div className='navbar'>
+
+        {/* SearchBar */}
+        <div className='search_icons'>
+          <nav>
+            <div className="nav-wrapper">
+              <form>
+                <div className="input-field">
+                  <input id="search" type="search" placeholder='Recherche' required />
+                  <label className="label-icon" for="search">
+                    <i className="material-icons">search</i>
+                  </label>
+                  <i className="material-icons">close</i>
+                </div>
+              </form>
+            </div>
+          </nav>
+        </div>
+        {/* SearchBar */}
+
+        {/* Tabs */}
+        <div className='tabs'>
+          <a href="#tab1">Boutique</a>
+          <a href="#tab2">De 0 à 3 ans</a>
+          <a href="#tab3">De 3 à 6 ans</a>
+          <a href="#tab4">De 6 à 10 ans</a>
+          <a href="#tab5">Plus de 10 ans</a>
+        </div>
+        {/* Tabs */}
+
+        {/* Icons Group */}
         <div className='group_icons'>
           <i class="material-icons">favorite_border</i>
           <i class="material-icons">person_outline</i>
           <i class="material-icons">shopping_cart</i>
         </div>
+        {/* Icons Group */}
+
       </div>
+      {/* General Bar */}
     </header>
   );
 };
