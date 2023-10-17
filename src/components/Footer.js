0bca52304589dc911c6c5c7f1f3af909bbe217fb
Clement <dumasclementjulien@yahoo.fr>
2023-10-17 17:25:31 +0200

    Modifs footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,37 +2,59 @@ import React from 'react';
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
-
     const footerStyle = {
-        backgroundColor: "#fdb0aa", // Définit la couleur de fond
-        color: "white", // Définit la couleur du texte en blanc
+        backgroundColor: "#fdb0aa",
+        color: "white",
+    };
+
+    const containerStyle = {
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr", // Deux colonnes égales
+        alignItems: "center",
+        gap: "20px", // Espace entre les colonnes
+        padding: "20px 0",
+    };
+
+    const socialIconsStyle = {
+        display: "flex",
+        alignItems: "center", // Centrer les icônes horizontalement
+    };
+
+    const ulStyle = {
+        textAlign: "center", // Aligner la liste à gauche
+    };
+
+    const iconStyle = {
+        marginRight: "50px",
+        marginLeft: "50px", // Ajoute de la marge à droite de l'icône de Facebook
     };
 
     return (
-        <footer className="page-footer" style={footerStyle}>
-            <div className="container">
-                <div className="row">
-                    <div className="col l4 offset-l2 s12">
-                        <ul>
-                            <li><a className="grey-text text-lighten-3" href="#!">Conditions générales de vente</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Données personnelles</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Mentions légales</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Nous contacter</a></li>
-                        </ul>
-                    </div>
-                    <div className="col l6 s12">
-                        <h5 className="white-text">Rejoignez la team Papou !</h5>
-                        <div className='social_icons'>
-                            <a href="https://www.facebook.com/yourpage"><FaFacebook size={50} color="white" /></a>
-                            <a href="https://www.instagram.com/yourpage"><FaInstagram size={50} color="white" /></a>
-                            <a href="https://www.linkedin.com/in/yourprofile"><FaLinkedin size={50} color="white" /></a>
-                        </div>
-                    </div>
+        <footer style={footerStyle}>
+            <div style={containerStyle}>
+                <div className="general_links">
+                    <ul style={ulStyle}>
+                        <li><a className="grey-text text-lighten-3" href="#!">Conditions générales de vente</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Données personnelles</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Mentions légales</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">Nous contacter</a></li>
+                    </ul>
+                </div>
+                <div className='icons_social' style={socialIconsStyle}>
+                    <a href="https://www.facebook.com/yourpage">
+                        <FaFacebook size={50} color="white" style={iconStyle} />
+                    </a>
+                    <a href="https://www.instagram.com/yourpage">
+                        <FaInstagram size={50} color="white" style={iconStyle} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/yourprofile">
+                        <FaLinkedin size={50} color="white" style={iconStyle} />
+                    </a>
                 </div>
             </div>
             <div className="footer-copyright">
-                <div className="container">
-                    © 2023 Copyright Text
+                <div style={{ textAlign: "center" }}>
+                    © 2023 Papou
                 </div>
             </div>
         </footer>
