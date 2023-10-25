85875eae8690955b261e237e3c414c72fd232c77
Clement <dumasclementjulien@yahoo.fr>
2023-10-25 18:54:00 +0200

    Modifs shop

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ProductList from './ProductList';
 import toy1 from '../../data/fake_toys_papou/toy_1.jpg';
-// import toy2 from '../data/fake_toys_papou/toy_2.jpg';
-// import toy3 from '../data/fake_toys_papou/toy_3.jpg';
-// import toy4 from '../data/fake_toys_papou/toy_4.jpg';
-// import toy5 from '../data/fake_toys_papou/toy_5.jpg';
-// import toy6 from '../data/fake_toys_papou/toy_6.jpg';
+import toy2 from '../../data/fake_toys_papou/toy_2.jpg';
+import toy3 from '../../data/fake_toys_papou/toy_3.jpg';
+import toy4 from '../../data/fake_toys_papou/toy_4.jpg';
+import toy5 from '../../data/fake_toys_papou/toy_5.jpg';
 
 const Shop = () => {
     const products = [
@@ -21,24 +20,28 @@ const Shop = () => {
             name: 'Produit 2',
             price: 29.99,
             description: 'Ceci est la description du produit 2.',
+            image: toy2
         },
         {
             id: 2,
             name: 'Produit 2',
             price: 29.99,
             description: 'Ceci est la description du produit 2.',
+            image: toy3
         },
         {
             id: 2,
             name: 'Produit 2',
             price: 29.99,
             description: 'Ceci est la description du produit 2.',
+            image: toy4
         },
         {
             id: 2,
             name: 'Produit 2',
             price: 29.99,
             description: 'Ceci est la description du produit 2.',
+            image: toy5
         },
         // Ajoutez d'autres produits ici
     ];
