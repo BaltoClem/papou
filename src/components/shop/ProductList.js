0ccdba67800d5049623eb73bf56d098cb7733f0e
Clement <dumasclementjulien@yahoo.fr>
2023-10-30 11:34:57 +0100

    CSS modifié pour ProductList + responsive

--- src/components/shop/ProductList.js
+++ src/components/shop/ProductList.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import '../../styles/shop/ProductList.css';
+
 
 const ProductList = ({ products }) => {
   return (
     <div>
       <div className="row">
         {products.map((product) => (
-          <div className="col s3 m3 l3" key={product.id}>
+          <div className="col s12 m6 l3" key={product.id}>
             <div className="card">
               <div className="card-image">
                 <img src={product.image} alt={product.name} />
@@ -14,6 +17,8 @@ const ProductList = ({ products }) => {
                 <p>Nom : {product.name}</p>
                 <p>Prix : {product.price} €</p>
                 <p>Description : {product.description}</p>
+                <br/>
+                <a href="/product-details" className="product_details">Voir le produit</a>
               </div>
             </div>
           </div>
